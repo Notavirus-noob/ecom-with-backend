b56d3c36a1eeca210ea74e2d54a192cfea7653cf
Notavirus-noob <100276041+Notavirus-noob@users.noreply.github.com>
2024-12-05 08:55:36 +0545

    all

---
+++ footer.php
@@ -0,0 +1,44 @@
+<footer class="section_p1">
+        <div class="col">
+            <a href="index.php">
+                <img class="logo" src="img/logo.png" alt="logo">
+            </a>    
+            <h4>Contact</h4>
+            <p><strong>Address:</strong> Bishal Bazar, New Road, Kathmandu</p>
+            <p><strong>Hours:</strong>8:00am to 7:00pm, all week</p>
+            <div class="follow">
+                <h4>Follow Us</h4>
+                <div class="icon">
+                    <i class="bi bi-facebook"></i>
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-youtube"></i>
+                    <i class="bi bi-pinterest"></i>
+                    <i class="bi bi-twitter-x"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="about.php">About us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms and Conditions</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="signuplogin.html">Sign In</a>
+            <a href="cart.php">View Cart</a>
+            <a href="#">Track My Order</a>
+            <a href="#">Help</a>
+        </div>
+
+        <div class="col install">
+            <p>Secured Payment Gateway</p>
+            <img src="img/pay/pay.png" alt="">
+        </div>
+
+        <div class="copyright">
+            <p>&copy; 2024 developed by BridgeCourier. </p> 
+        </div>
+    </footer>
